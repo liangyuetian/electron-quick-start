bf365ba2de7400fbb4b9925c29d38640f3d8824f
王闯 <pt_liangyue@outlook.com>
2018-12-22 16:31:26 +0800

    添加mac打包

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let mainWindow
 function createWindow() {
   Menu.setApplicationMenu(null)
   // Create the browser window.
-  mainWindow = new BrowserWindow({ width: 800, height: 600, transparent: true })
+  mainWindow = new BrowserWindow({ width: 800, height: 600})
 
   // and load the index.html of the app.
   mainWindow.loadFile('dist/index.html')
